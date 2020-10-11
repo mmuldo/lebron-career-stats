12e3e94f31e93c2b459f31640baa00280506e15f
Charlie Minaya <charlieminaya18@gmail.com>
2020-10-11 01:18:23 -0400

    css

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home_page():
 def about_page():
     # takes user to about page
     # brief information of what this webapp is about with addition of images/animations
-    return 'About'
+    return "this page will have information about what this site does"
 
 
 @app.route('/lebron')
@@ -22,14 +22,13 @@ def lebron_page():
     # takes user to lebron page(to start off with, soon this will be players url if we advance
     # will contain image, name, height, age, ethnicity, hometown, salary,
     # teams
-    return 'Lebron'
-
+    return "this page will have general information about Lebron James"
 
 @app.route('/lakers')
 def lakers_page():
     # takes user to lakers page (soon to be teams page) containing the teams in their divisions
     # in alphabetical order with the year of its founding
-    return 'Lakers'
+    return "this page will have information on Lebron's current team: Lakers"
 
 
 @app.route('/stats')
@@ -41,4 +40,4 @@ def stats_page():
 @app.route('/trivia')
 def trivia_page():
     # takes user to about page
-    return 'Trivia'
+    return "this page will have a 10 question trivia to see how much the user knows Lebron"
